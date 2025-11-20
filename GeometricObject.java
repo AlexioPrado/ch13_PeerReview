b9018e2bebd2aac13b892f9b181a15cc48f59918
Marcus Alexio Prado <mgprado1@my.waketech.edu>
2025-11-20 10:17:26 -0500

    help

--- GeometricObject.java
+++ GeometricObject.java
@@ -3,10 +3,6 @@ public abstract class GeometricObject {
   private boolean filled;
   private java.util.Date dateCreated;
 
-  
-
-
-
   /** Construct a default geometric object */
   protected GeometricObject() {
     dateCreated = new java.util.Date();
--- Test.java
+++ Test.java
@@ -1,4 +1,7 @@
 
+import com.sun.jdi.Value;
+
+
 public class Test {
   // Main method
   public static void main(String[] args) {
@@ -14,18 +17,69 @@ public class Test {
   }
 }
 
-abstract class GeometricObject1 implements Comparable {
-  public Circle1 max(Circle1 one, Circle1 two) {
-    if (one.getRadius() > two.getRadius()){
-      return one;
-    } else {
-      return two;
-    }
+public abstract class GeometricObject implements Comparable {
+  private String color = "white";
+  private boolean filled;
+  private java.util.Date dateCreated;
+
+  /** Construct a default geometric object */
+  protected GeometricObject() {
+    dateCreated = new java.util.Date();
   }
-} 
+
+  /** Construct a geometric object with color and filled value */
+  protected GeometricObject(String color, boolean filled) {
+    dateCreated = new java.util.Date();
+    this.color = color;
+    this.filled = filled;
+  }
+
+  /** Return color */
+  public String getColor() {
+    return color;
+  }
+
+  /** Set a new color */
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  /** Return filled. Since filled is boolean,
+   *  the get method is named isFilled */
+  public boolean isFilled() {
+    return filled;
+  }
+
+  /** Set a new filled */
+  public void setFilled(boolean filled) {
+    this.filled = filled;
+  }
+
+  /** Get dateCreated */
+  public java.util.Date getDateCreated() {
+    return dateCreated;
+  }
+
+  @Override
+  public String toString() {
+    return "created on " + dateCreated + "\ncolor: " + color +
+      " and filled: " + filled;
+  }
+
+  /** Abstract method getArea */
+  public abstract double getArea();
+
+  /** Abstract method getPerimeter */
+  public abstract double getPerimeter();
+
+  public abstract Object max(Object, Object);
+
+}
+
+
 
 // Circle.java: The circle class that extends GeometricObject
-class Circle1 extends GeometricObject1 { 
+class Circle1 extends GeometricObject { 
   private double radius = 0;
 
   public Circle1() {}
@@ -38,10 +92,6 @@ class Circle1 extends GeometricObject1 {
     this.radius = radius;
   }
 
-  public double getArea(){
-    return Math.pow(this.radius, 2) * Math.PI;
-  }
-
   public double getRadius(){
     return this.radius;
   }
@@ -49,6 +99,26 @@ class Circle1 extends GeometricObject1 {
   public double getDiameter(){
     return this.radius * 2;
   }
+
+  @Override
+  public double getArea(){
+    return Math.pow(this.radius, 2) * Math.PI;
+  }
+
+  @Override
+  public double getPerimeter(){
+    return this.radius* 2 * Math.PI;
+  }
+
+  @Override
+  public Circle1 max(Circle1 one, Circle1 two){
+    if (one.getRadius() > two.getRadius()){
+      return one;
+    } else {
+      return two;
+    }
+  }
+
 }
 
 
